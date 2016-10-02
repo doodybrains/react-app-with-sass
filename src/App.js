dcf684cf7586e7716a5d27594f61d23cd01a9af9
Emma Norton <doodybrainz@gmail.com>
2016-10-02 15:46:49 -0400

    Initialize with sass

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+require('./app.scss');
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>New App With Sass</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
